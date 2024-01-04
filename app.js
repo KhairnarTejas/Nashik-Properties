88f1700cbe13338353c9992ddea033a27d4ca50b
KhairnarTejas <tejasdk2004@gmail.com>
2024-01-04 11:05:51 +0530

    database connect commit

--- app.js
+++ app.js
@@ -1,11 +1,27 @@
+if (process.env.NODE_ENV != "production") {
+    require('dotenv').config();
+}
+
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 
-app.get("/", (req,res) => {
+dbUrl = process.env.ATLASDB_URL;
+
+main()
+    .then(() => {
+        console.log("Connected to db.");
+    })
+    .catch(err => console.log(err));
+
+async function main() {
+    await mongoose.connect(dbUrl);
+}
+
+app.get("/", (req, res) => {
     res.send("Welcome");
 })
 //Pull
-app.listen(8080, (req,res) => {
+app.listen(8080, (req, res) => {
     console.log("Server is listening to port");
 });
