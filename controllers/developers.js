616ba5f52c6ddcad53182c1c2c93886191b9ca98
KhairnarTejas <tejasdk2004@gmail.com>
2024-01-07 18:20:45 +0530

    projectsfrom developer

--- controllers/developers.js
+++ controllers/developers.js
@@ -1,4 +1,6 @@
 const Developer = require("../models/developer");
+const Listing = require("../models/listing");
+
 
 module.exports.index = async (req,res)=> {
     const allDevelopers = await Developer.find({});
@@ -31,9 +33,44 @@ module.exports.updateDeveloper = async (req,res) => {
     res.redirect("/developers");
 }
 
+
 module.exports.destroyDeveloper = async (req, res) => {
-    const {id} = req.params;
-    const deletedDeveloper = await Developer.findByIdAndDelete(id);
-    console.log(deletedDeveloper);
-    res.redirect("/developers");
-}
+    const { id } = req.params;
+
+    try {
+        // Find the developer and get the listing IDs
+        const developer = await Developer.findById(id).populate("sites");
+        const listingIds = developer.sites.map((listing) => listing._id);
+
+        // Delete the listings
+        await Listing.deleteMany({ _id: { $in: listingIds } });
+
+        // Delete the developer
+        const deletedDeveloper = await Developer.findByIdAndDelete(id);
+        console.log("Deleted Developer:", deletedDeveloper);
+
+        res.redirect("/developers");
+    } catch (error) {
+        console.error(error);
+        res.status(500).send("Internal Server Error");
+    }
+};
+
+module.exports.showProjectsFromDeveloper = async (req, res) => {
+    try {
+        let { id } = req.params;
+        const allListings = await Listing.find({ project_by: id });
+
+        const developer = await Developer.findById(id);
+
+        if (!developer) {
+            console.log("Developer not found");
+            return res.status(404).send("Developer not found");
+        }
+
+        res.render("developers/projectsfromdeveloper.ejs", { data: { allListings, id, developer } });
+    } catch (error) {
+        console.error(error);
+        res.status(500).send("Internal Server Error");
+    }
+};
