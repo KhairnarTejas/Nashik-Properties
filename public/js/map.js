122dda6432daaf8884e741adcb94c85c9a56168c
KhairnarTejas <tejasdk2004@gmail.com>
2024-09-15 23:54:44 +0530

    update map.js

--- public/js/map.js
+++ public/js/map.js
@@ -1,6 +1,6 @@
 console.log(mapToken);
 mapboxgl.accessToken ='pk.eyJ1Ijoic2lkZGhhcnRoMDAwMjciLCJhIjoiY2xxNHF3dHQ3MGI4ZzJwbGhidm4xcXpxNyJ9.HRP80FyJvTuJrvCagzw8Aw';
-mapboxgl.accessToken = mapToken;
+// mapboxgl.accessToken = mapToken;
 const map = new mapboxgl.Map({
     container: 'map', // container ID
     center: listing.geometry.coordinates, // starting position [lng, lat]
